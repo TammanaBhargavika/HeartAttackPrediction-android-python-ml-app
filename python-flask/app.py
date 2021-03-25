38f99ed2d9ce828c2982e557c1792fdfbdd1f35c
Your Name <you@example.com>
2021-03-25 20:28:28 +0530

    ChangedOnMar25

--- python-flask/app.py
+++ python-flask/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, redirect, url_for, jsonify, request,render_template
-import pickle
 import numpy as np
+import pickle
+
+from flask import Flask, redirect, url_for, jsonify, request,render_template
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
-
 
 @app.route('/')
 def index():
@@ -15,34 +15,48 @@ def ff():
 
 @app.route('/predict',methods = ['POST', 'GET'])
 def predict():
-   # age gender trp cholestrol fbs ecg thalaz exong old peak slope
+   # Age Gender Trp Cholestrol Fbs Ecg Thalach Exang OldPeak Slope
    values=[]
+   
    name=request.form['name']
    #values.append(name)
+   
    age=request.form['age']
    values.append(age)
+   
    sex=request.form['sex']
    values.append(sex)
+
    cp=request.form['cp']
    values.append(cp)
+
    trp=request.form['trp']
    values.append(trp)
+
    cholestrol=request.form['cholesterol']
    values.append(cholestrol)
+
    fbs=request.form['fbs']
    values.append(fbs)
+
    ecg=request.form['Ecg']
    values.append(ecg)
+
    Thalaz=request.form['Thalaz']
    values.append(Thalaz)
+
    Exong=request.form['Exong']
    values.append(Exong)
+
    Oldpeak=request.form['Old Peak']
    values.append(Oldpeak)
+
    slope=request.form['slope']
    values.append(slope)
+
    ca=request.form['ca']
    values.append(ca)
+
    thal=request.form['thal']
    values.append(thal)
    
